06a7d9a004992e55acadce729f27d3c90adca7b8
rumana <kadri.rumana@gmail.com>
2026-03-10 14:52:25 +0000

    added cors to app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //express framework
 //Maps to the specific Routes based on the url or parameters
 const express = require ('express')
+const cors = required('cors')
 const fruitsApp = express()
 const fruitRoutes = require('./routes/fruitRoutes')
 
+fruitsApp.use(cors())
 fruitsApp.use(express.json()) //any HTTP Method
 fruitsApp.use('/fruits', fruitRoutes)
 
